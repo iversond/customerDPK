83b0d33401eb342bef613f1a7a43cb164116b547
Dan Iverson <dan@daniverson.com>
2025-07-10 13:31:27 -0500

    Fix typo and image URL

--- scripts/signon.sh
+++ scripts/signon.sh
@@ -3,7 +3,10 @@ echo "------------"
 echo "Updating Logo on Signon Page for web domains"
 
 echo " -- Downloading Image from Github"
-curl -L -o /tmp/OracleLogo_Black.svg https://github.com/iversond/customerDPK/blob/b1164c73a9e5e025f2f43e1d5aedda2f97b037e6/scripts/images/InsertLogoHere-Header.png
+curl -L -o /tmp/OracleLogo_Black.svg https://raw.githubusercontent.com/iversond/customerDPK/main/scripts/images/InsertLogoHere-Header.png
 
 echo "-- Copying to all PS_CFG_HOME/werbserv sites"
 sudo -u psadm2 -i sh -c "cp /tmp/OracleLogo_Black.svg \${PS_CFG_HOME}/webserv/*/applications/peoplesoft/PORTAL.war/*/images/"
+
+echo "-- Restart web server for changes"
+sudo -u opc -i sh -c "psa restart web"
